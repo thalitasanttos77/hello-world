68d210105779efc9335a4e171f48fc92a85d3563
thalita <thalitasanttos77@gmail.com>
2025-03-22 16:05:06 -0300

    Initial commit

---
+++ Teste/teste.js
@@ -0,0 +1,5 @@
+var x = 10 + 20;
+console.log(x);
+var idade = 20;
+var nome = "Thalita";
+console.log("Meu nome \u00E9 ".concat(nome, " e tenho ").concat(idade, " anos."));
---
+++ hello-world/src/teste.ts
@@ -0,0 +1,8 @@
+let x = 10 + 20;
+console.log(x);
+
+let idade: number = 20;
+let nome: string = "Thalita";
+
+// Correção: usar crases (`) em vez de aspas (' ou ")
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
