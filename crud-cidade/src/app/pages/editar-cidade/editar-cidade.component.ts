e10692ec9ea604d9bd552a78453715dcfd3beb31
thalita <thalitasanttos77@gmail.com>
2025-05-25 21:24:19 -0300

    implementa combox

--- crud-cidade/src/app/pages/editar-cidade/editar-cidade.component.ts
+++ crud-cidade/src/app/pages/editar-cidade/editar-cidade.component.ts
@@ -4,24 +4,33 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Cidade } from '../../shared/models/cidade.model';
 import { CidadeService } from '../../services/cidade.service';
-
+import { NgSelectModule } from '@ng-select/ng-select';
+import { EstadoService } from '../../services/estado.service';
+import { Estado } from '../../shared/models/estado.model';
 
 @Component({
   selector: 'app-editar-cidade',
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, NgSelectModule],
   templateUrl: './editar-cidade.component.html',
   styleUrl: './editar-cidade.component.css'
 })
 export class EditarCidadeComponent implements OnInit {
 @ViewChild('formCidade') formCidade! : NgForm;
 cidade: Cidade = new Cidade();
+estados: Estado[] = [];
+
+
 constructor(
   private cidadeService: CidadeService,
+  private estadoService: EstadoService,
   private route: ActivatedRoute,
   private router: Router
+  
 ){}
 
 ngOnInit(): void {
+
+    this.estados=this.estadoService.listarTodos();
    
       let id = +this.route.snapshot.params['id'];
 
@@ -30,8 +39,7 @@ ngOnInit(): void {
           this.cidade = res;
         else
           throw new Error ("Cidade não encontrada: id =" + id);
-    }
-      
+  }      
     atualizar(): void {
     // Verifica se o formulário é válido
       if (this.formCidade.form.valid) {
--- crud-cidade/src/app/pages/inserir-cidade/inserir-cidade.component.ts
+++ crud-cidade/src/app/pages/inserir-cidade/inserir-cidade.component.ts
@@ -1,24 +1,33 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormsModule, NgForm} from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm, NgSelectOption} from '@angular/forms';
 import { Cidade } from '../../shared/models/cidade.model';
 import { CidadeService } from '../../services/cidade.service';
 import { Router, RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { EstadoService } from '../../services/estado.service';
+import { Estado } from '../../shared/models/estado.model';
 
 @Component({
   selector: 'app-inserir-cidade',
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, NgSelectModule],
   templateUrl: './inserir-cidade.component.html',
   styleUrl: './inserir-cidade.component.css'
 })
-export class InserirCidadeComponent {
+export class InserirCidadeComponent implements OnInit{
   @ViewChild('formCidade') formCidade! : NgForm;
   cidade : Cidade = new Cidade();
+  estados: Estado[] = [];
 
   constructor(
     private cidadeService : CidadeService, 
+    private estadoService: EstadoService,
     private router: Router
+
   ){}
+  ngOnInit(): void {
+    this.estados=this.estadoService.listarTodos();
+  }
 
   inserir(): void {
     if (this.formCidade.form.valid) {
