bb22b26fad84be08c62b75b0e5ef577b48c7bd44
thalita <153288534+thalitasanttos77@users.noreply.github.com>
2025-08-26 09:20:04 -0300

    Delete Teste directory

--- Teste/teste.js
+++
@@ -1,5 +0,0 @@
-var x = 10 + 20;
-console.log(x);
-var idade = 20;
-var nome = "Thalita";
-console.log("Meu nome \u00E9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- Teste/teste.ts
+++
@@ -1,7 +0,0 @@
-let x = 10 + 20;
-console.log(x);
-
-let idade: number = 20;
-let nome: string = "Thalita";
-
-console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
